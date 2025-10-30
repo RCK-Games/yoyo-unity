d2e59b045ea1f0d0a8956ca99cb8b9ae0f9234ba
Dylan Ocampo <dylanomar@live.com.mx>
2025-10-30 15:27:00 -0600

    Animation changes

--- Assets/Scripts/Interfaces/MenuButtonInterface.cs
+++ Assets/Scripts/Interfaces/MenuButtonInterface.cs
@@ -20,7 +20,10 @@ public class MenuButtonInterface : MonoBehaviour
 
     public void handleAnimation()
     {
-        gameObject.transform.DOScale(0.8f, 0.3f);
+        gameObject.transform.DOScale(0.8f, 0.3f).onComplete += () =>
+        {
+            gameObject.transform.DOScale(1f, 0.3f);
+        };
         buttonIcon.transform.DORotate(new Vector3(0, 0, 45), 0.3f);
         if(placesViewModel != null)
         {
@@ -35,7 +38,6 @@ public class MenuButtonInterface : MonoBehaviour
     
     public void closeAnimation()
     {
-        gameObject.transform.DOScale(1f, 0.3f);
         buttonIcon.transform.DORotate(new Vector3(0, 0, 0), 0.3f);
     }
 }
